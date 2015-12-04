8dcae360abfeeec72170a944121ab5a7e8c6965d
Martin Hoffmann <hn@nvnc.de>
2015-12-04 10:47:49 +0100

    Move domain module out again.

--- src/domain/mod.rs
+++
@@ -1,6 +0,0 @@
-
-pub use self::error::{Result, Error};
-
-pub mod error;
-pub mod name;
-//pub mod rr;
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 extern crate nickel;
 extern crate xml;
 
-pub mod domain;
 pub mod droplets;
 pub mod storage;
